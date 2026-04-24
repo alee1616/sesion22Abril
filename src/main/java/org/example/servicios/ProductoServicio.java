14596ed2fe3e4e9705737ca176e9c725ae12f2d7
alee1616 <maguillenc@uamv.edu.ni>
2026-04-24 16:36:06 -0600

    eliminar producto

--- src/main/java/org/example/servicios/ProductoServicio.java
+++ src/main/java/org/example/servicios/ProductoServicio.java
@@ -8,31 +8,59 @@ import java.util.List;
 
 public class ProductoServicio implements ProductoInterface {
 
-    private List<Producto> productos;
+    private final List<Producto> productos;
 
     public ProductoServicio() {
         this.productos = new ArrayList<>();
     }
 
     @Override
-    public void agregarProducto(String nombre, int cantidad, double precio) {
+    public void agregarProducto(String nombre, double precio, int cantidad) {
         this.productos.add(new Producto(nombre, cantidad, precio));
+    }
+
+    @Override
+    public void eliminarProducto(String nombre) {
+        productos.removeIf(producto -> producto.getNombre().equalsIgnoreCase(nombre));
+    }
+
+    @Override
+    public void modificarProducto(String nombreOriginal, String nuevoNombre, double nuevoPrecio, int nuevaCantidad) {
+        for (Producto producto : productos) {
+            if (producto.getNombre().equalsIgnoreCase(nombreOriginal)) {
+                producto.setNombre(nuevoNombre);
+                producto.setPrecio(nuevoPrecio);
+                producto.setCantidad(nuevaCantidad);
+                break;
+            }
+        }
+    }
+
+    @Override
+    public List<Producto> getProductos() {
+        return productos;
+    }
+
+    /**
+     * Calcula el monto total a pagar por todos los productos en la lista.
+     * @return monto total
+     */
+    public double getMonto() {
+        double monto = 0;
+        for (Producto producto : productos) {
+            double total = producto.getCantidad() * producto.getPrecio();
+            monto += total;
+        }
+        return monto;
+    }
+
+    @Override
+    public void agregarProducto(String nombre, int cantidad, double precio) {
 
     }
 
     @Override
     public List<Producto> getroductos() {
-        return productos;
-    }
-
-    public double getMonto(){
-        double total = 0, monto = 0;
-        for(Producto producto : productos){
-            total = producto.getCantidad() * producto.getPrecio();
-            monto += total;
-        }
-
-        return monto;
-
+        return List.of();
     }
 }
